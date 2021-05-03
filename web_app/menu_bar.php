c80598c9649597ae433d192b98fd805c481ab0c9
MrG-DevCode <81931731+MrG-DevCode@users.noreply.github.com>
2021-05-03 11:50:01 +0700

    Add files via upload

--- web_app/menu_bar.php
+++ web_app/menu_bar.php
@@ -3,19 +3,21 @@
 <link rel="stylesheet" type="text/css" href="../css/menu_bar.css">
 <title></title>
 </head>
-<body>
-<div class="menu_bar">
-<div class="menu_option">
-<a href=""><li>Home |</li></a>
-<a href=""><li>Panduan |</li></a>
-<a href=""><li>Contact Us |</li></a>
-</div>
-<form c1ass="login" action="#" method="post">
-<input type="text" size="7" name="id" placeholder="Username"></input>
-<input type="password" size="7" name="pass" placeholder="Password"></input>
-<input type="submit" value="Login" ></input>
-<input type="submit" value="Daftar"></input>
-</form>
-</div>
-</body>
+
+    <body>
+        <div class="menu_bar">
+            <div class="menu_option">
+                <a href=""><li>Home |</li></a>
+                <a href=""><li>Panduan |</li></a>
+                <a href=""><li>Contact Us |</li></a>
+            </div>
+
+        <form class="login" action="#" method="post">
+        <input type="text" size="7" name="id" placeholder="Username"></input>
+        <input type="password" size="7" name="pass" placeholder="Password"></input>
+        <input type="submit" value="Login" ></input>
+        <input type="submit" value="Daftar"></input>
+        </form>
+        </div>
+    </body>
 </html>
